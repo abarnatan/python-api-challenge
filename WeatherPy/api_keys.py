0366a1266c32fc05b9e2aef197e9511a40dd18fa
abarnatan <ale.barn19@gmail.com>
2020-10-31 16:48:48 -0700

    api call working, dataframe created from call

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "28dd5bd9fdcd0365d2a88aabf63d3125"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCNV-NRxat8UiP7qT3BuZFLNE9gDkakhzI"
